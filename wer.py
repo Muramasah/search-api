25ef63b75cdc740faa37fa579c887060ca5ece81
Felipe Medina <medina.xavier.felipe@gmail.com>
2021-11-04 17:26:02 -0300

    Creates API Application based

--- wer.py
+++ wer.py
@@ -1,26 +1,10 @@
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from src.Application import app
 
+if __name__ == '__main__':
+    host_name = 'localhost'
+    server_port = 8888
 
-class WERRequestHandler(BaseHTTPRequestHandler):
-    """This class handles HTTP request for the WER service."""
+    app.run(debug=True, host=host_name, port=server_port)
 
-    def do_GET(self):
-        """Handles GET requests."""
-        self.send_response(200)  # TODO: Replace with actual logic.
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(f"<h1>WER service online</h1>".encode('utf-8'))
-
-
-if __name__ == "__main__":
-    hostName = 'localhost'
-    serverPort = 8888
-    server = HTTPServer((hostName, serverPort), WERRequestHandler)
-    print(f"Server started at {hostName}:{serverPort}")
-    try:
-        server.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    server.server_close()
     print("Server stopped")
