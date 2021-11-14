dd0eaaa485067ea2713c7e2d2cb6e700628a3f5e
Felipe Medina <medina.xavier.felipe@gmail.com>
2021-11-13 21:19:56 -0300

    Implements test for FindWebsitesByQuery use case

---
+++ Domain/UseCases/test_find_websites_by_query.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+from unittest.mock import MagicMock
+
+from Domain.UseCases.FindWebsitesByQuery import FindWebsitesByQuery
+
+
+class TestFindWebsitesByQuery(TestCase):
+    '''Test FindWebsitesByQuery use case'''
+
+    def setUp(self) -> None:
+        # If we don't mock the class dependencies we are testing those
+        # dependencies as well.
+        self.mocked_search_engine = MagicMock()
+
+        self.use_case = FindWebsitesByQuery(self.mocked_search_engine)
+
+        return super().setUp()
+
+    def test_execute_should_return_website_dtos(self):
+        filter_dto = {'query': 'query'}
+        result_dtos = {'title': 'title', 'url': 'url'}
+        self.mocked_search_engine.find_all_by_query.return_value = result_dtos
+
+        returned_result_dtos = self.use_case.execute(filter_dto)
+
+        # Here we are assuring that the repository is called with the query
+        self.mocked_search_engine.find_all_by_query.assert_called_with(
+            filter_dto['query'])
+        # Here we are assuring that the use case is returning the repository 
+        # result.
+        self.assertDictEqual(returned_result_dtos, result_dtos)
